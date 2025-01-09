cc1edc53aced4bd8fadb6e89acd2fb61aee1be09
Dang-Cao-Cuong <cuong.dangcao.cit22@eiu.edu.vn>
2025-01-09 15:14:37 +0700

    Add UserList

---
+++ UserList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class UserList {
+    public List<User> userList = new ArrayList<>();
+    public void addUser(User u){
+        this.userList.add(u);
+    }
+    public void removeUser(User u){
+        this.userList.remove(u);
+    }
+
+}
