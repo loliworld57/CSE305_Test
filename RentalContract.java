13f1f2e799f541c73071a87b93c023820c493182
Dang-Cao-Cuong <cuong.dangcao.cit22@eiu.edu.vn>
2025-01-09 16:08:34 +0700

    Update RentalContract.java

--- RentalContract.java
+++ RentalContract.java
@@ -4,5 +4,10 @@ public class RentalContract {
     public int propertyID;
     public int tenantID;
     public long rentAmount;
-
+    public RentalContract(int id, int pid,int tid,long a){
+        this.contractID = id;
+        this.propertyID = pid;
+        this.tenantID = tid;
+        this.rentAmount = a;
+    }
 }
