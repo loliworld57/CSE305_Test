667db86ca3f67491603371e6fa56318b970d587d
Tung Hoang <loliworld57@gmail.com>
2025-01-09 15:40:21 +0700

    Change Rental and Tenant

--- RentalContract.java
+++ RentalContract.java
@@ -10,12 +10,5 @@ public class RentalContract {
         this.tenantID = tid;
         this.rentAmount = a;
     }
-<<<<<<< Updated upstream
-    public void createNewContract(int id, int pid,int tid,long a){
-        RentalContract rc = new RentalContract(id, pid, tid, a);
-=======
-    public void createNewContract(int id, Property p,Tenant t,long a){
-        RentalContract rc = new RentalContract(id, p.propertyID, t.u.userid, a);
->>>>>>> Stashed changes
-    }
+
 }
