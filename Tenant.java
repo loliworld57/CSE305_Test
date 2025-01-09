c5ce294eb320a84b46498d0dcd74dd0712c6b652
Dang-Cao-Cuong <cuong.dangcao.cit22@eiu.edu.vn>
2025-01-09 15:58:33 +0700

    Update Tenant.java

--- Tenant.java
+++ Tenant.java
@@ -4,5 +4,10 @@ import java.util.*;
 public class Tenant {
     public User u;
     public List<RentalContract> listRental;
-
+    public void createRentalContract(RentalContract rt){
+        listRental.add(rt);
+    }
+    public void removeRentalContract(RentalContract rt){
+        listRental.remove(rt);
+    }
 }
