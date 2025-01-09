092ecceac0f2c4dc8bf9714be3571e46d259b149
Tung Hoang <loliworld57@gmail.com>
2025-01-09 15:51:24 +0700

    Import

--- Tenant.java
+++ Tenant.java
@@ -1,8 +1,10 @@
 import java.util.*;
-
+import User.java;
 
 public class Tenant {
-    public User u;
+    public User user;
     public List<RentalContract> listRental;
-
+    public Tenant(User u){
+        this.user = u;
+    }
 }
